55881d9243dcc88d2ae71d2fd0416fea54ad71e8
egithubland <info@edataland.com>
2019-08-20 14:28:26 -0700

    Update subscriptions.js

--- src/graphql/subscriptions.js
+++ src/graphql/subscriptions.js
@@ -7,6 +7,7 @@ export const onCreateTodo = `subscription OnCreateTodo {
     name
     description
     status
+    createdAt
   }
 }
 `;
@@ -16,6 +17,7 @@ export const onUpdateTodo = `subscription OnUpdateTodo {
     name
     description
     status
+    createdAt
   }
 }
 `;
@@ -25,6 +27,7 @@ export const onDeleteTodo = `subscription OnDeleteTodo {
     name
     description
     status
+    createdAt
   }
 }
 `;
